77b6654f16e59e7fa40a24f30173f08c77a02a90
Nicolette <nderespino@gmail.com>
2023-10-23 18:55:00 -0400

    added function to allow one click per mole

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 module.exports = {
   verbose: false,
   preset: "jest-puppeteer",
   //testEnvironment: "node",
 };
+=======
+module.exports = {
+  verbose: false,
+  preset: "jest-puppeteer",
+  //testEnvironment: "node",
+};
+>>>>>>> 5af2160b6193662ef4630e247642f183a43f7388
